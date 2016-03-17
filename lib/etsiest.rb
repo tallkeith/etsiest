30348b8427d2639d76426654bf60d4117f14b69e
tallkeith <tallestkeith@gmail.com>
2016-03-17 16:00:40 -0400

    Started building out classes and added requriements

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,5 +1,19 @@
+require "sinatra/base"
+require "json"
+require "pry"
+
 require "etsiest/version"
+require 'etsy' 
+
+Etsy.api_key = ENV['ETSY_KEY']
 
 module Etsiest
-  # Your code goes here...
+	class Etsy < Sinatra::Application
+
+		get "/search" do 
+
+		end
+
+		run! if app_file == $0
+	end
 end
